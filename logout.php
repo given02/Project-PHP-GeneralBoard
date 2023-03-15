d47865c3a001c3c2179bbba513fc8afc2ca5dbc6
given02 <39878215+given02@users.noreply.github.com>
2023-03-15 16:02:22 +0900

    boardPHP commit

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+	include  $_SERVER['DOCUMENT_ROOT']."/db.php";
+	session_destroy();
+?>
+<meta charset="utf-8">
+<script>
+    alert("로그아웃되었습니다."); 
+    location.href="/login.php"; 
+</script>
